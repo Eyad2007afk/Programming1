5e27c8d8a6761692e6d2192fec861918d3690a69
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-04 15:55:14 +1100

    Create lesson01.c
    intro to C

---
+++ lesson01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+#include <stdbool.h>
+
+int main()
+{
+    printf("Helllo world!\n");
+
+    printf("size of int = %zu bytes\n", sizeof(int));
+    printf("maximum int value= %d\n", INT_MAX);
+
+    return 0;
+
+}
