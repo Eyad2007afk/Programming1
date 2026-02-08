aeb5485f4e0c7cd4d2ce02a29b128e426fc3cbf1
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-09 09:32:17 +1100

    Update range_classifier.c

--- range_classifier.c
+++ range_classifier.c
@@ -1,57 +1,20 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <limits.h>
+//nclude <stdbool.h>
 
-int main() {
-    int choice = 1; // 1, 2, 3
+int main(){
+    int a;
+    scanf("%d", &a);
 
-    if (choice == 1) {
-        printf("Choice 1\n");
-    } else if (choice == 2) {
-        printf("Choice 2\n");
-    } else if (choice == 3) {
-        printf("Choice 3\n");
-    } else {
-        printf("Error: invalid choice!\n");
-        return 1;
+    if (a < 30){
+        printf("Your mark is : D \n");
+    } else if (a < 50 && a > 59){
+        printf("Your mark is : C\n");
+    } else if ( a < 60 && a > 69){
+        printf("Your mark is : B\n");
+    } else if (a < 70){
+        printf("Your mark is : A\n");
     }
-
-    switch (choice) {
-        case 1:
-            puts("Choice 1 in S/C");
-            break;
-        case 2:
-            puts("Choice 2 in S/C");
-            break;
-        case 3:
-            puts("Choice 3 in S/C");
-            break;
-        default:
-            printf("Error: invalid choice!\n");
-            return 1;
-    }
-
-    // Dangling else
-    int user = 0;
-    bool isAdmin = false;
-
-    // Note that user 2 is admin
-
-    // puts("Enter your user number: ");
-    // scanf("%d", &user);
-
-    /*
-     *      Be careful: C does not mind
-     *      if you forget braces on your if
-     */
-
-    // if (user == 2)
-    //     puts("User 2...");
-
-    //     isAdmin = true;
-
-    // if (isAdmin) {
-    //     puts("Super user!");
-    // }
-
     return 0;
 }
+
