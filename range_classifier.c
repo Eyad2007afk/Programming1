5ed3247851cd17d5f3c253525952eefd8f438289
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-10 16:30:14 +1100

    Update range_classifier.c
    final edition of this question

--- range_classifier.c
+++ range_classifier.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <limits.h>
-//nclude <stdbool.h>
 
 int main(){
     int a;
     scanf("%d", &a);
-
-    if (a < 30){
-        printf("Your mark is : D \n");
-    } else if (a < 50 && a > 59){
-        printf("Your mark is : C\n");
-    } else if ( a < 60 && a > 69){
-        printf("Your mark is : B\n");
-    } else if (a < 70){
-        printf("Your mark is : A\n");
+    if (a <= 30){
+        printf("You suck bruh omg\n");
+    } else if (a >= 30 && a <= 50) {
+        printf("maybe you dont suck after all...take a C\n");
+    } else if (a >= 50 && a <= 60){
+        printf("Dam it looks like your good after all, you deserve a B\n");
+    } else if (a >= 60 && a <= 70){
+        printf("Okay you're good dawg, take a B+\n");
+    } else if (a >= 70 && a <= 100){
+        printf("you the goat, straight A\n");
     }
     return 0;
 }
-
