37a76caf9fafa83ec10352e3c584266179891a2a
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-13 12:18:36 +1100

    Create math_util.h

---
+++ math_util.h
@@ -0,0 +1,12 @@
+#ifndef MATH_UTIL_H
+#define MATH_UTIL_H
+
+int add (int a, int b);
+int times (int a, int b);
+int subtract (int a, int b);
+int multiply(int a, int b);
+float divide (int a,int b);
+int power(int base,unsigned int nonneg_power);
+unsigned long long factorial_rec ( int n);
+
+#endif
