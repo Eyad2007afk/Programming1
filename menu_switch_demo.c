85140997c83873b94f58c135daba1455d22801e5
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-16 15:37:09 +1100

    finished with week 1 work thankfully

--- menu_switch_demo.c
+++ menu_switch_demo.c
@@ -3,27 +3,32 @@
 #include <limits.h>
 
 int main(){
-    while (1){
+    
     printf("Choose please kind sir :  \n");
     printf(" 1) add\n 2) subtract\n 3) quit\n");
-    int a;
-    scanf("%d", &a);
+    
+    int a, b,c;
+    printf("and then choose another two numbers please : \n");
+    scanf("%d %d %d", &a, &b,&c);
     
     switch (a){
         case 1 :
-            printf ("you're going to add....\n"); 
+            printf ("you're going to add....\n");
+            printf("The addition of your chosen numbers is : %d\n", b +c); 
             return 0;
         case 2 : 
             printf("you're going to be subtracting...\n");
+            printf("The subraction of your chosen numbers is : %d\n", b - c); 
+
             return 0;
         case 3 :
             printf("you're going to quit...\n");
             return 0;
         default : 
             printf("You chose wrong mi amigo, rerun this program please...\n");
-            
-            
-            
         }
+
     }
-}
+    
+
+
