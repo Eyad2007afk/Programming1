f32d4825edec82368e27cc9fadff2f707da2fbda
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-02-19 19:22:21 +1100

    on another roll, started lesson 4

---
+++ week_2/fizzbuzz_run.c
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+
+
+int main(){
+    printf("enter a number gang : \n");
+    int a; 
+    scanf("%d", &a);
+    printf("the number you chose is : %d\n", a);
+    if (a % 2 ==0 ){
+        printf("the number is even\n ");
+    } else if (a % 2 != 0 ){
+        printf("the number is odd\n");
+    }
+
+    printf("check this out \n");
+    printf("type 1 : \n");
+    int b;
+    scanf("%d",&b );
+    for (int i = 1; i <= 16; i++){
+        if (i % 2==0){
+            printf("number %d is even\n", i);
+        } else if (i % 2 !=0){
+            printf("number %d is odd\n", i);
+        }
+    }
+    return 0;   
+}
