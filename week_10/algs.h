8981217207ce135925c3667590d89f49c75f272f
Eyad2007afk <9958257@schoolsnet.act.edu.au>
2026-04-24 22:54:10 +1000

    work from home 24/4

---
+++ week_10/algs.h
@@ -0,0 +1,79 @@
+#ifndef ALGS_H
+#define ALGS_H 
+
+#include <stdio.h>
+#include <time.h>
+
+//TIMING LOGIC 
+void test(int n) {
+    int sum = 0;
+    for  (int i = 0; i < n; i++) {
+        sum += i;
+      
+    }
+}
+
+//SEARCH ALGORITHMS 
+
+    //LINEAR SEARCH 
+    int linear_search(const int *a, size_t n, int target){
+    if (!a || n == 0 ) return -1;
+    for(size_t i = 0; i < n; i++){
+        if (a[i] == target) return (int) i ;
+    }
+    return -1;
+}
+
+    //BINARY SEARCH 
+    int binary_search(const int *a, size_t n, int target){
+        if (!a || n == 0) return -1;
+        size_t lo = 0, hi = n;
+        while (lo < hi){
+            size_t mid = lo + (hi - lo) / 2;
+            int v = a[mid];
+            if (v < target){
+                lo = mid +1;
+            } else {
+                hi = mid;
+            }
+        }
+        if (lo < n && a[lo] == target) return (int) lo;
+        return -1;
+    }
+
+
+//ELEMENTARY SORTS I 
+
+    //SELECTION SORT 
+    void swap_ints(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selection_sort (int *a, size_t n ){
+    if (!a || n < 2) return;
+    for (size_t i = 0; i < n; i++){
+        size_t min_i = i;
+        for (size_t j = i+ 1; j < n; ++j){
+            if (a[j] < a[min_i]) min_i = j;
+        } 
+        if (min_i != i ) swap_ints(&a[i], &a[min_i]);
+    }
+}
+
+    //INSERTION SORT 
+    void insertion_sort(int *a, size_t n){
+        if (!a || n < 2) return;
+        for (size_t i = 1; i < n ; ++i){
+            int target = a[i];
+            size_t j = i;
+            while (j > 0 && a[j - 1] > target){
+                a[j] = a[j-1];
+                --j;
+            }
+            a[j] = target;
+        }
+    }
+
+    #endif
--- week_10/linear_search_simpl.c
+++ week_10/linear_search_simpl.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include "algs.h"
 
-int linear_search(const int *a, size_t n, int target){
-    if (!a || n == 0 ) return -1;
-    for(size_t i = 0; i < n; i++){
-        if (a[i] == target) return (int) i ;
-    }
-    return -1;
-}
+
 int main (void){
     int sizes[11] = {1,55,23,4,8,67,200,11,9,0,5};
     int result = linear_search(sizes,11, 55);
     printf("searching through this algorithm : %d\n", result);
-    return 0;
+    
     //CHECK THE TIME TAKEN FOR EXTRA KNOWLEDGE 
+   
+        clock_t start = clock();
+        test(result);
+        clock_t end = clock();
+        double time_taken = (double) (end - start) / CLOCKS_PER_SEC;
+        printf("time taken to search :  %f\n", result, time_taken);
+  return 0;  
 }
+
